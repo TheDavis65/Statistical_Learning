5edfcf684a98f9be1192c077097ec67ed8ca9591
TheDavis65 <michaeldavis@mail.dk>
2021-09-18 17:16:29 +0200

    initial commit Statistical Learning

---
+++ sl1.R
@@ -0,0 +1,23 @@
+
+pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes,
+               ggvis,httr, lubridate, plotly, rio, rmarkdown, shiny,
+               stringr, tidyr, caret, lars, tidyverse, psych, dygraphs,
+               vioplot, gapminder, nycflights13, gapminder, Lahman, ISLR2 )
+
+summary(Smarket)
+lm(Today~Lag1+Lag2,data=Smarket)
+head(Smarket)
+plot(Smarket)
+head(Wage)
+head()
+
+library(readr)
+Advertising <- read_csv("Data/Advertising.csv")
+View()
+Advertising
+plot(Advertising)
+
+tv <- select(Advertising,"TV","sales")
+head(tv)
+view(tv)
+plot(tv)
